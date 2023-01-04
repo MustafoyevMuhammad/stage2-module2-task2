8b9f4b8ce864ab11f43bf67e5dc24ad47419b342
Mustafoyev <mmustafoyev9@gmail.com>
2023-01-04 16:28:34 +0500

    commit message

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,19 +1,19 @@
-//package com.example.listener;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletContextEvent;
-//import javax.servlet.ServletContextListener;
-//import javax.servlet.annotation.WebListener;
-//import javax.servlet.http.HttpSession;
-//import javax.servlet.http.HttpSessionListener;
-//import java.time.LocalDateTime;
-//
-//@WebListener
-//public class ContextListener implements ServletContextListener {
-//    @Override
-//    public void contextInitialized(ServletContextEvent event) {
-//        ServletContext context = event.getServletContext();
-//        context.setAttribute("servletTimeInit", LocalDateTime.now());
-//    }
-////write your code here!
-//}
+package com.example.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext context = event.getServletContext();
+        context.setAttribute("servletTimeInit", LocalDateTime.now());
+    }
+//write your code here!
+}
